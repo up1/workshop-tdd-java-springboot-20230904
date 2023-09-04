cdfa8470db488f0b914e0279a98bfc4224253516
up1 <somkiat.p@gmail.com>
2023-09-04 15:08:33 +0700

    Config junit5 + mockitp 5

--- workshop-testing/src/test/java/IdGeneratorServiceMockitoTest.java
+++ workshop-testing/src/test/java/IdGeneratorServiceMockitoTest.java
@@ -1,17 +1,23 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+@ExtendWith(MockitoExtension.class)
 class IdGeneratorServiceMockitoTest {
+
+    @Mock
+    private MyRandom random;
 
     @Test
     @DisplayName("ต้องการ generate id = START-5")
     public void case01() {
         // Arrange
         // Stub
-        MyRandom random = null;
         IdGeneratorService service = new IdGeneratorService();
         // Act
         String result = service.process(random);
