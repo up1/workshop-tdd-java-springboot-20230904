d114366aed6cac970f1dc2098a1c83f9ff916d50
up1 <somkiat.p@gmail.com>
2023-09-04 14:03:33 +0700

    Split succcess and failcase

--- workshop-testing/src/test/java/MyProblemSuccessTest.java
+++ workshop-testing/src/test/java/MyProblemSuccessTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MyProblemTest {
+class MyProblemSuccessTest {
 
     @Test
     @DisplayName("เริ่มด้วย [")
@@ -104,29 +104,5 @@ class MyProblemTest {
         assertEquals("12345", result);
     }
 
-    @Test
-    @DisplayName("input ไม่ถูกต้อง คือ 1,5] => throw InputInvalidException")
-    public void case10() {
-        // Arrange
-        MyProblem myProblem = new MyProblem("1,5]");
-        // Act
-        try {
-            myProblem.process();
-            fail("InputInvalidException not found !!");
-        } catch (InputInvalidException e) {
-            assertEquals("Error case01", e.getMessage());
-        }
-
-    }
-
-    @Test
-    @DisplayName("Junit 5 => input ไม่ถูกต้อง คือ 1,5] => throw InputInvalidException")
-    public void case11() {
-        // Arrange
-        MyProblem myProblem = new MyProblem("1,5]");
-        // Act and Assert
-        Exception e = assertThrows(InputInvalidException.class, myProblem::process);
-        assertEquals("Error case01", e.getMessage());
-    }
 
 }
