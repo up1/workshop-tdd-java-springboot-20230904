0777ec7432f8272854cb48df7a498e516940e1d7
up1 <somkiat.p@gmail.com>
2023-09-04 09:53:13 +0700

    Hello with junit

---
+++ workshop-testing/src/test/java/HelloTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    @DisplayName("input อะไร ทำอะไร แล้วต้องได้อะไร")
+    public void success_with_expected_result() {
+
+    }
+
+    @Test
+    public void successWithExpectedResult() {
+
+    }
+
+}
