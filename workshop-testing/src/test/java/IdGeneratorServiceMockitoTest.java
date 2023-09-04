67c04d71168f1565d4612e769117e7de1f16f41b
up1 <somkiat.p@gmail.com>
2023-09-04 15:18:08 +0700

    Working with mockito

--- workshop-testing/src/test/java/IdGeneratorServiceMockitoTest.java
+++ workshop-testing/src/test/java/IdGeneratorServiceMockitoTest.java
@@ -5,6 +5,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,7 +18,7 @@ class IdGeneratorServiceMockitoTest {
     @DisplayName("ต้องการ generate id = START-5")
     public void case01() {
         // Arrange
-        // Stub
+        when(random.nextInt(10)).thenReturn(5);
         IdGeneratorService service = new IdGeneratorService();
         // Act
         String result = service.process(random);
