b1fde7ca7b3c7be966c003b4357f1c00663dc193
up1 <somkiat.p@gmail.com>
2023-09-05 12:01:06 +0700

    Try to test api

--- api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
+++ api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerMockMvcTest {
@@ -18,6 +21,8 @@ class UserControllerMockMvcTest {
     private MockMvc mvc;
 
     @Test
-    void getUserById() {
+    void getUserById() throws Exception {
+        this.mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
