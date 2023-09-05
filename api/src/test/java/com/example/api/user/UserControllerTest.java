aa6d9b11c9a4c1ee658a85f0db7c439836603b44
up1 <somkiat.p@gmail.com>
2023-09-05 11:11:18 +0700

    Testing with spring boot test

---
+++ api/src/test/java/com/example/api/user/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.api.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class UserControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void getUserById() {
+    }
+}
