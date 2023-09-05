97b90cc9435069c04a44ad12520015756f9a5c7b
up1 <somkiat.p@gmail.com>
2023-09-05 14:45:58 +0700

    Create gateway to call external api

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate xxx() {
+		return new RestTemplateBuilder().build();
 	}
 
 }
---
+++ api/src/main/java/com/example/api/user/UserGateway.java
@@ -0,0 +1,31 @@
+package com.example.api.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Component
+public class UserGateway {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Optional<UserGatewayResponse> callApi(int id) {
+        try {
+            UserGatewayResponse result = restTemplate.getForObject(
+                    "https://jsonplaceholder.typicode.com/users/" + id,
+                    UserGatewayResponse.class);
+            return Optional.ofNullable(result);
+        } catch (RestClientException e) {
+            return Optional.empty();
+        }
+    }
+
+}
+
+class UserGatewayResponse {
+
+}
