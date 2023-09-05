0036f3ecbad69baec76e427140e019ed57ea3c67
up1 <somkiat.p@gmail.com>
2023-09-05 11:52:05 +0700

    Use mockbean to create a mock object

--- api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
+++ api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
@@ -3,12 +3,16 @@ package com.example.api.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerMockMvcTest {
+
+    @MockBean
+    private UserService userService;
 
     @Autowired
     private MockMvc mvc;
