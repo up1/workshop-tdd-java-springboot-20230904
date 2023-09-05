8897d314709eb2554e3f71f02df4090be82aee9f
up1 <somkiat.p@gmail.com>
2023-09-05 13:14:34 +0700

    Check response body

--- api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
+++ api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerMockMvcTest {
@@ -21,8 +21,23 @@ class UserControllerMockMvcTest {
     private MockMvc mvc;
 
     @Test
-    void getUserById() throws Exception {
+    void getUserByIdWithSuccess() throws Exception {
         this.mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+    @Test
+    void getUserByIdWithSuccessAndCheckData() throws Exception {
+        // Arrange
+        UserResponse expected = new UserResponse();
+        expected.setId(1);
+        expected.setFirtname("Somkiat");
+        expected.setLastname("Pui");
+        when(userService.getById(1)).thenReturn(expected);
+        // Act and Assert
+        this.mvc.perform(get("/user/1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.firtname").value("Somkiat"))
+                .andExpect(jsonPath("$.lastname").value("Pui"));
     }
 }
